302c5252a20c7ff2959dc00b2f20d56db39f842d
Pratyaksh Mathur <pratyaksh.mathur28@gmail.com>
2025-09-06 15:50:24 -0500

    update

--- rosalind_problems.py
+++
@@ -1,9 +0,0 @@
-# Enter code for Rosalind 2 here:
-string = "Fwzew1KvqEmN4PrbSrHRQVMVc68SOK6MhZFbyZ9WAjZ3FZfZ9Ui3Gi7GJjRzJRegulusfbD2PUEhEjTvwj2cygnus9UMuYhNuIZtel2kSwxhTiCbr0Lpi6xVd1bhHZ4B9RIyQv1mKHuml9lAtjOBwGmsVqMM0uF."
-a = 61
-b = 67
-c = 83
-d = 88
-print (string[a:b+1], string[c:d+1])
-
-   
